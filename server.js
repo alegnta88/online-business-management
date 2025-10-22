0d79158a71141b7d573af3bb292d66db0eed44f2
root <root@alegnta.digaf.local>
2025-10-22 11:20:49 +0300

    server.js issues fixed

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import connectToDatabase from './config/env.js';
 import connectCloudinary from './config/cloudinary.js';
