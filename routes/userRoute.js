ff4d9befa0c799b24f9d4771d23717f5ed83352b
root <root@alegnta.digaf.local>
2025-11-03 14:58:22 +0300

    user route issues

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { registerUser, verifyOTP, loginUser, adminLogin, getAllUsers } from '../controllers/userController.js';
 import adminAuth from '../middleware/adminAuth.js';
+import userAuth from '../middleware/userAuth.js';
 
 
 const userRouter = express.Router();
 
 
-userRouter.post('/register', adminAuth, registerUser);
+userRouter.post('/register', registerUser);
 userRouter.post('/verify', verifyOTP);
 userRouter.post('/login', loginUser);
 userRouter.post('/admin', adminLogin)
